9ad1d3c63d0e448e7cd1d54549ed782ef09232ce
Sam Joseph <sjoseph92@gmail.com>
2019-07-20 16:12:21 -0700

    [feat] added promotional header to navbar

---
+++ client/src/components/PromoHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PromoHeader = () => 
+    <div className="promo-header">
+        <div className="promo-container">
+            <div className="promo-banner">
+                <p className="promo-text">up to 30% off bedding</p>
+                <p className="promo-text">up to 30% off rugs</p>
+                <p className="promo-text">20% off accent chairs</p>
+                <p className="promo-text">30% off emmerson collection</p>
+                <p className="promo-text">up to 30% off lighting + more</p>
+            </div>
+        </div>
+    </div>
+
+export default PromoHeader
