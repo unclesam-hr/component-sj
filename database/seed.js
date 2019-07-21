91adfb75fdd286386ac082f6aedb12f5791651b9
Sam Joseph <sjoseph92@gmail.com>
2019-07-21 15:40:54 -0700

    [feat] refactored seed.js to accom. new collection along with its seeding data

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const models = require('./index.js');
 
+mongoose.connection.collections['collectionName'].drop( function(err) {
+    console.log('collection dropped');
+});
+
+
 const categorySeedData = [
   { name: "Living Room" },
   { name: "Sofa & Sectional Collections" },
@@ -109,6 +114,86 @@ const descriptionSeedData = [
     {name: "metal"}
 ]
 
+var searchObjects = 
+[
+{
+name: 'sofas',
+categories:['Living Room','Sofa & Sectional Collections','Outdoor Furniture','Clearance'],
+descriptions: [ 'garden','baby','kids','accent','urban','outdoor','patio','indoor','patterned','solid','moroccan','persian','natural','white','neutral','gray','blue','purple','pink','abstract','modern','leather','wood','rattan','silk','mid-century','bamboo','wicker','small space','vintage']
+},
+{
+name: 'sectionals',
+categories:['Living Room','Sofa & Sectional Collections','Outdoor Furniture','Clearance'],
+descriptions: [ 'garden','baby','kids','accent','urban','side','outdoor','patio','indoor','patterned','solid','moroccan','persian','natural','white','neutral','gray','blue','purple','pink','abstract','modern','leather','wood','rattan','silk','mid-century','bamboo','wicker','small space','vintage']
+},
+{
+name: 'loveseats',
+categories:['Living Room','Collections','Outdoor Furniture','Clearance'],
+descriptions: [ 'garden','baby','kids','accent','urban','outdoor','patio','indoor','patterned','solid','moroccan','persian','natural','white','neutral','gray','blue','purple','pink','modern','leather','rattan','silk','mid-century','bamboo','wicker','small space','vintage']
+},
+{
+name: 'furniture',
+categories: [ 'Living Room','Sofa & Sectional Collections','Dining Room & Kitchen','Kitchen furniture','Bedroom','Home Office','More Rooms','Outdoor Furniture','Kitchen','Brands','Clearance'],
+descriptions: ['garden','dining','bar','kitchen','office','baby','kids','accent','urban','outdoor','patio','indoor','entryway','hallway','modern','leather','rattan','mid-century','bamboo','wicker','plastic','small space','vintage']
+},
+{
+name: 'chairs',
+categories: [ 'Living Room','Dining Room & Kitchen','Home Office','More Rooms','Outdoor Furniture'],
+descriptions: ['garden','dining','bar','counter','kitchen','desk','office','baby','kids','accent','urban','side','outdoor','patio','indoor','neutral','gray','blue','purple','pink','abstract','modern','rattan','mid-century','bamboo','wicker','plastic','vintage']
+},
+{
+name: 'coffee tables',
+categories: [ 'Living Room','More Rooms','Outdoor Furniture','Clearance'],
+descriptions:  ['glass','garden','urban','side','outdoor','patio','indoor','abstract','modern','rattan','mid-century','bamboo','wicker','plastic','metal','small space','vintage']
+},
+{
+name: 'side tables',
+categories: [ 'Living Room','More Rooms','Outdoor Furniture','Clearance'],
+descriptions:  ['glass','garden','urban','side','outdoor','patio','indoor','abstract','modern','rattan','mid-century','bamboo','wicker','plastic','metal','small space','vintage']
+},
+{
+name: 'tables',
+categories: [ 'Living Room','Dining Room & Kitchen','Kitchen Furniture','Home Fffice','Outdoor Furniture','Clearance'],
+descriptions:  ['glass','garden','dining room','bar','counter','kitchen','office','accent','urban','side','outdoor','patio','indoor','rattan','silk','mid-century','bamboo','wicker','plastic','small space','vintage','coffee','side','console']
+},
+{
+name: 'bar stools',
+categories:[ 'Living Room','Dining room & Kitchen','Kitchen Furniture','Outdoor Furniture','Bar & Glassware'],
+descriptions:  ['urban','outdoor','patio','indoor','abstract','modern','leather','wood','rattan','mid-century','bamboo','wicker','plastic','metal','vintage']
+},
+{
+name: 'bar carts',
+categories:[ 'Living Room','Dining room & Kitchen','Kitchen Furniture','Outdoor Furniture','Bar & Glassware'],
+descriptions: ['glass','urban','outdoor','patio','indoor','abstract','modern','leather','wood','rattan','mid-century','bamboo','wicker','plastic','metal','vintage']
+},
+{
+name: 'wall art',
+categories: ['Living Room','Dining Room & Kitchen','Bedroom','Home Office','More Rooms','Outdoor Pillows & Decor','Planters & Garden','Room Accents','Accessories & Gifts','All Art','Art by Color','Art by Subject','Clearance'],
+descriptions: ['glass','office','decorative','abstract','modern','mid-century','vintage']
+},
+{
+name: 'rugs',
+categories: ['Living Room','Bedroom','Outdoor','Bath Linens','Bath Linen Collections','Clearance'],
+descriptions: ['kitchen','kids','accent','urban','outdoor','patio','indoor','patterned','solid','moroccan','persian','natural','entryway','hallway','organic cotton','white','neutral','gray','blue','purple','pink','floor','abstract','modern','mid-century','small space','vintage'] 
+},
+{
+name: 'lighting',
+categories: ['Living Room','Dining Room & Kitchen','Bedroom','Home Office','Bathroom Hardware & Lighting','All Lighting','Lighting Collections','Clearance'],
+descriptions: ['bath','garden','accent','urban','outdoor','patio','indoor','decorative','modern','mid-century','vintage']
+},
+{
+name: 'lamps',
+categories: ['Living Room','Dining Room & Kitchen','Bedroom','Home Office','Bathroom Hardware & Lighting','All Lighting','Lighting Collections','Clearance'],
+descriptions: ['garden','accent','urban','outdoor','patio','indoor','decorative','modern','mid-century','vintage','floor']
+},
+{
+name: 'pillows',
+categories: ['Living Room','Bedroom','Outdoor Pillows & Decor','All Bedding','Bedding Essentials','All Bedding Essentials','Baby & Kids Bedding','Pillows & Throws & Poufs','All Pillows, Throws & Poufs','Room Accents','Clearance'],
+descriptions: ['baby','kids','accent','urban','side','outdoor','patio','indoor','patterned','solid','moroccan','decorative','linen','organic cotton','abstract','modern','silk','mid-century','vintage']
+}
+
+]
+
 const seedCategoryFunction = () => {
     models.Category.create(categorySeedData)
     .then( () => {
@@ -127,5 +212,17 @@ const seedDescFunction = () => {
     .catch( err => console.error(err));
 }
 
+const seedSearchObjFunction = () => {
+    models.Description.create(searchObjects)
+    .then( () => {
+        console.log('Database seeded!');
+        mongoose.connection.close();
+    })
+    .catch( err => console.error(err));
+}
+
+
+
 seedCategoryFunction();
 seedDescFunction();
+seedSearchObjFunction()
