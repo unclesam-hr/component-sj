687ebc67d95f50079059eaabb3878bde6a6a1819
Sam Joseph <sjoseph92@gmail.com>
2019-07-22 08:34:46 -0700

    [feat] refactored search algorithim to create random results if no match found

--- server/index.js
+++ server/index.js
@@ -14,20 +14,94 @@ app.use(parser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 
-app.get("/api/search", cors(), (req, res) => {
+app.post("/api/search", cors(), (req, res) => {
 
-    Promise.all([dbHelpers.getAllCategories(), dbHelpers.getAllDesc()])
-        .then((result) => {
-            var catResult = result[0]
-            var descResult = result[1]
-            var x = Math.floor(Math.random() * (catResult.length - 1))
-            var y = Math.floor(Math.random() * (catResult.length - 1))
-            var z = Math.floor(Math.random() * (catResult.length - 1))
 
+    Promise.all([dbHelpers.getAllCategories(), dbHelpers.getAllDesc(), dbHelpers.getAllObjs()])
+        .then((results) => {
+            var categories = results[0]
+            var descriptions = results[1]
+            var searchObjs = results[2]
+            var searchWord = req.body.text
             
-            
-            res.send([catResult[x].name, catResult[y].name, catResult[z].name ,descResult[x].name, descResult[y].name, descResult[z].name])
-        })
+            var createSearchResults = function(word) {
+                var results = [];
+                var searchResultObjs = searchObjs
+                
+              
+                var autoComplete = function(word) {
+                  var wordLength = word.length
+                
+                    for (var i = 0; i < searchResultObjs.length; i++) {
+                      var arrayEle = searchResultObjs[i].name.slice(0, wordLength)
+                      if(word === arrayEle) {
+                        return searchResultObjs[i]
+                      }
+                    }
+                  return -1
+                }
+              
+                var resultsFormatter = function(obj) {
+                  var catSearchResults = []
+                  var descSearchResults = []
+                  
+                  while(catSearchResults.length !== 3) {
+                    var randomCatIndex = Math.floor(Math.random()*obj.categories.length)
+                    catSearchResults.push(`${obj.name} in ${obj.categories[randomCatIndex]}`)
+                    obj.categories.splice(randomCatIndex, 1)
+                  }
+                
+                  while(descSearchResults.length !== 5) {
+                    var randomDescIndex = Math.floor(Math.random()*obj.descriptions.length)
+                    descSearchResults.push(`${obj.descriptions[randomDescIndex]} ${obj.name}`)
+                    obj.descriptions.splice(randomDescIndex, 1)
+                  }
+                
+                  
+                  descSearchResults.unshift(obj.name)
+                  results.push(...catSearchResults)
+                  results.push(...descSearchResults)
+                  return results
+                  }
+
+                  var resultRandomizer = function(word) {
+                      var catResults = [];
+                      var descResults = [];
+
+                    while(catResults.length !== 3) {
+                        var catIndex = Math.floor(Math.random() * categories.length)
+                        catResults.push(`${word} in ${categories[catIndex].name}`)
+                        categories.splice(catIndex, 1)
+                    }
+
+                    while(descResults.length !== 5) {
+                        var descIndex = Math.floor(Math.random() * descriptions.length)
+                        descResults.push(`${descriptions[descIndex].name} ${word}`)
+                        descriptions.splice(descIndex, 1)
+                    }
+
+                    descResults.unshift(word)
+                    results.push(...catResults)
+                    results.push(...descResults)
+                    return results
+                  }
+
+
+                
+              
+                if(autoComplete(searchWord) === -1) {
+                  resultRandomizer(searchWord)
+                  return results
+                } else {
+                  var completeWord = autoComplete(searchWord)
+                  resultsFormatter(completeWord)
+                  return results
+                }
+        }
+              
+         res.send(createSearchResults(searchWord))
+        
+         })
 
 })
 
